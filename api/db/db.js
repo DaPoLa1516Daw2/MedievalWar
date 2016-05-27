0b4a9847454082e7c2dbde4031c9df5d07693ffe
DaPoLa1516Daw2 <dapola1516daw2@gmail.com>
2016-05-27 16:15:05 +0200

    re

--- api/db/db.js
+++ api/db/db.js
@@ -6,15 +6,19 @@ const recorder = require('./plugins/recorder');
 
 
 let db = mongoose.createConnection(config.db.url);
-db.authenticate(config.db.user,config.db.password)
-db.on('open', () => {
-    console.log(`Connected to MongoDB at ${config.db.url}`);
+db.authenticate(config.db.user,config.db.password, function(err, res){
+
+
+    db.on('open', () => {
+        console.log(`Connected to MongoDB at ${config.db.url}`);
+    });
+
+    db.on('error', err => {
+        console.error(`Unable to connect to MongoDB at ${config.db.url}`);
+        throw err;
+    });
 });
 
-db.on('error', err => {
-    console.error(`Unable to connect to MongoDB at ${config.db.url}`);
-    throw err;
-});
 
 mongoose.plugin(recorder);
 
