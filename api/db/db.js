02304fa3074c552d0e410ee649c246a263a6ab09
DaPoLa1516Daw2 <dapola1516daw2@gmail.com>
2016-05-27 16:21:34 +0200

    re

--- api/db/db.js
+++ api/db/db.js
@@ -6,19 +6,15 @@ const recorder = require('./plugins/recorder');
 
 
 let db = mongoose.createConnection(config.db.url);
-db.authenticate(config.db.user,config.db.password, function(err, res){
 
-
-    db.on('open', () => {
-        console.log(`Connected to MongoDB at ${config.db.url}`);
-    });
-
-    db.on('error', err => {
-        console.error(`Unable to connect to MongoDB at ${config.db.url}`);
-        throw err;
-    });
+db.on('open', () => {
+    console.log(`Connected to MongoDB at ${config.db.url}`);
 });
 
+db.on('error', err => {
+    console.error(`Unable to connect to MongoDB at ${config.db.url}`);
+    throw err;
+});
 
 mongoose.plugin(recorder);
 
--- config/db.js
+++ config/db.js
@@ -7,7 +7,5 @@
 module.exports = {
 
     url: 'mongodb://localhost:27017/MedievalWar',
-    user: 'admin',
-    password: 'qkZJJj_mJLf6'
 
 };
